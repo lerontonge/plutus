1519f9e3307fa643b4f78477376910e136f6ec7c
Ziyang Liu <unsafeFixIO@gmail.com>
2026-03-06 14:52:21 -0800

    Allow the uplc executable to take serialised scripts (#7630)

---
+++ plutus-executables/plutus/Mode/HelpVersion.hs
@@ -0,0 +1,18 @@
+module Mode.HelpVersion
+  ( runHelp
+  , runVersion
+  ) where
+
+import Data.Version.Extras (gitAwareVersionInfo)
+import GetOpt
+import Paths_plutus_executables qualified as Paths
+
+runHelp :: IO ()
+runHelp = do
+  putStr $ GetOpt.usageInfo usageHeader GetOpt.optDescrs
+
+usageHeader :: String
+usageHeader = "USAGE: plutus [--compile|--run|--bench|--debug] FILES..."
+
+runVersion :: IO ()
+runVersion = putStrLn (gitAwareVersionInfo Paths.version)
